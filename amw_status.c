7c2b8a70eccdc86c4cf0ce5f1584cc9f02ea7078
petbrain <uebi@tutanota.com>
2025-04-11 05:26:09 +0000

    Changed dump to to_string.

--- amw_status.c
+++ amw_status.c
@@ -44,17 +44,25 @@ static void amw_status_hash(UwValuePtr self, UwHashContext* ctx)
     uw_ancestor_of(UwTypeId_AmwStatus)->hash(self, ctx);
 }
 
-static void amw_status_dump(UwValuePtr self, FILE* fp, int first_indent, int next_indent, _UwCompoundChain* tail)
+static UwResult amw_status_to_string(UwValuePtr self)
 {
     AmwStatusData* data = _amw_status_data_ptr(self);
 
-    _uw_dump_start(fp, self, first_indent);
-    _uw_dump_struct_data(fp, self);
+    char location[48];
+    snprintf(location, sizeof(location), "Line %u, position %u: ",
+             data->line_number, data->position);
 
-    UwValue desc = uw_ancestor_of(UwTypeId_AmwStatus)->to_string(self);
-    UW_CSTRING_LOCAL(desc_cstr, &desc);
-    fprintf(fp, " line %u, position %u: %s\n",
-            data->line_number, data->position, desc_cstr);
+    UwValue result = uw_create_string(location);
+    uw_return_if_error(&result);
+
+    UwValue status_str = uw_ancestor_of(UwTypeId_AmwStatus)->to_string(self);
+    uw_return_if_error(&status_str);
+
+    if (!uw_string_append(&result, &status_str)) {
+        return UwOOM();
+    }
+
+    return uw_move(&result);
 }
 
 static UwType amw_status_type;
@@ -63,10 +71,10 @@ static UwType amw_status_type;
 static void init_amw_status()
 {
     UwTypeId_AmwStatus = uw_subtype(&amw_status_type, "AmwStatus", UwTypeId_Status, AmwStatusData);
-    amw_status_type.create   = amw_status_create;
-    amw_status_type.init     = amw_status_init;
-    amw_status_type.hash     = amw_status_hash;
-    amw_status_type.dump     = amw_status_dump;
+    amw_status_type.create    = amw_status_create;
+    amw_status_type.init      = amw_status_init;
+    amw_status_type.hash      = amw_status_hash;
+    amw_status_type.to_string = amw_status_to_string;
 
     // init status codes
     AMW_END_OF_BLOCK = uw_define_status("END_OF_BLOCK");
